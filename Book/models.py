0730e47787909f104d2e2ee18dadb5e53792c0f5
AhmedYosry <a7med.elesawy0155066@gmail.com>
2021-05-16 20:26:57 +0200

    Done paginator , showe book list ,some edite in details page

--- Book/models.py
+++ Book/models.py
@@ -1,8 +1,17 @@
 from django.db import models
-
+from django.utils.text import slugify
 # Create your models here.
 
 class Book (models.Model):
     name = models.CharField(max_length =50)
-    descripton = models.TextField(max_length = 300)
-    auther = models.CharField(max_length = 30)
+    descripton = models.TextField(max_length = 600)
+    img = models.ImageField(upload_to ='Books')
+    count_of_page = models.IntegerField(blank=True , null = True)
+    count_of_download=models.IntegerField(blank=True , null = True)
+
+    file = models.FileField(upload_to='files/%Y/%m/%d')
+
+    def __str__(self):
+        return self.name
+
+   
--- Book/urls.py
+++ Book/urls.py
@@ -3,5 +3,6 @@ from . import views
 app_name = 'Book'
 
 urlpatterns=[
-    
+    path ('booklist/', views.book_list, name = 'booklist'),
+    path ('^(?P<id>\d+)$', views.book_details, name = 'bookdetails'),
 ]
