60caaddabde95f4d6a800e0dc85e9918dec1ae5c
AhmedYosry <a7med.elesawy0155066@gmail.com>
2021-05-20 21:00:36 +0200

    Add About Page , Active In Header ,Footer In App Team

--- team/views.py
+++ team/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
+# Team Page
 def team(request):
     
     all_team=Team.objects.all() 
@@ -11,3 +11,8 @@ def team(request):
         'team':all_team
         }
     return render(request, 'team.html',context)
+
+#About Page
+def about(request):
+    
+    return render (request,'about.html')
