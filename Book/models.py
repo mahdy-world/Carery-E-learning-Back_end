743f296fbf4b9d269a38309ffae519d5d3d60e06
AhmedYosry <a7med.elesawy0155066@gmail.com>
2021-05-20 09:51:19 +0200

    download file done in book

--- Book/models.py
+++ Book/models.py
@@ -15,24 +15,24 @@ class Book (models.Model):
     def __str__(self):
         return self.name
 
-    register = template.Library()
+    # register = template.Library()
 
-    def sizify(value):
-        """
-        Simple kb/mb/gb size snippet for templates:
+    # def sizify(value):
+    #     """
+    #     Simple kb/mb/gb size snippet for templates:
         
-        {{ product.file.size|sizify }}
-        """
-        #value = ing(value)
-        if value < 512000:
-            value = value / 1024.0
-            ext = 'kb'
-        elif value < 4194304000:
-            value = value / 1048576.0
-            ext = 'mb'
-        else:
-            value = value / 1073741824.0
-            ext = 'gb'
-        return '%s %s' % (str(round(value, 2)), ext)
+    #     {{ product.file.size|sizify }}
+    #     """
+    #     #value = ing(value)
+    #     if value < 512000:
+    #         value = value / 1024.0
+    #         ext = 'kb'
+    #     elif value < 4194304000:
+    #         value = value / 1048576.0
+    #         ext = 'mb'
+    #     else:
+    #         value = value / 1073741824.0
+    #         ext = 'gb'
+    #     return '%s %s' % (str(round(value, 2)), ext)
 
-    register.filter('sizify', sizify)
+    # register.filter('sizify', sizify)
