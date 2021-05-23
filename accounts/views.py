74ae74e3ffb71843584f9e88da11cdd663b58bd8
dawnbergdev <mmos7239@gmail.com>
2021-05-23 16:06:24 +0200

    sign_up done

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import *
+
 
 # Create your views here.
-
+def signup(request):
+    if request.method=="POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username,password=password)
+            login(request,user)
+            return redirect('/Auth/home')
+    else:
+        form = SignupForm()
+    return render(request,'registration/sign_up.html',{'form':form})
