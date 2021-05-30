2035b787229aebcea5c2c11790bc9a51f385bfaf
ahmedyosry433 <a7med.elesawy0155066@gmail.com>
2021-05-30 16:34:16 +0200

    Book Done counter

--- Book/models.py
+++ Book/models.py
@@ -15,15 +15,15 @@ class Book (models.Model):
     name = models.CharField(max_length =50 ,  verbose_name = 'اسم الكتاب')
     category = models.ForeignKey(Category,related_name = "Books_category" ,on_delete=models.CASCADE , verbose_name = 'قسم الكتاب')
     descripton = models.TextField(max_length = 600, verbose_name = 'تفاصيل عن الكتاب')
-    img = models.ImageField(upload_to ='Books', verbose_name = 'صورة الكتاب')
-    count_of_page = models.IntegerField(blank=True , null = True, verbose_name = 'عدد الصفحات')
+    img = models.ImageField(upload_to ='Books',default= 'default.png' ,verbose_name = 'صورة الكتاب')
+    slid2 =models.ImageField(upload_to ='Books', default= 'default.png',verbose_name = ' الصوره الثانيه')
+    slid3 =models.ImageField(upload_to ='Books', default= 'default.png',verbose_name = ' الصوره الثالثه')
     count_of_download=models.IntegerField(blank=True , null = True, verbose_name = 'عدد التحميلاات')
 
     file = models.FileField(upload_to='files/%Y/%m/%d', verbose_name = 'رفع الملف')
+    url = models.CharField(max_length=500, blank=True, null=True)
 
     def __str__(self):
         return self.name
-
-    def increment_numdownload(self):
-        self.count_of_download +=1
-        return self.count_of_download
+    
+  
--- Book/urls.py
+++ Book/urls.py
@@ -1,3 +1,4 @@
+from os import name
 from django.urls import path
 from . import views
 app_name = 'Book'
@@ -5,5 +6,7 @@ app_name = 'Book'
 urlpatterns=[
     path ('all', views.book_list, name = 'BookList'),
     path ('books/<int:pk>/detail', views.book_details, name = 'bookdetails'),
+    path('download/<int:id>', views.download2, name="downloadfun"),
+     path('show/<int:id>', views.show_pdf, name="show"),
     
 ]
